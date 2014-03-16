b9a7e22a21f7386ff5ee96f3aeff224262ad455b
Ben Keith <baynkiz@gmail.com>
2014-03-16 18:50:24 +0100

    TU ET index.html fonctionnent (avec http mock en TU)

--- web/Test1/app/js/controllers.js
+++ web/Test1/app/js/controllers.js
@@ -3,12 +3,14 @@
  */
 var hybridDemoApp = angular.module('hybridDemoApp',[]);
 
-hybridDemoApp.controller('vmListCtrl',function ($scope) {
-	$scope.vms= [
+hybridDemoApp.controller('vmListCtrl',function ($scope,$http) {
+	$scope.oldvms= [ 
 	             {'name':'apache1', 'network':'Clt1-CW-priv1', 'status':'ACTIVE'},
 	             {'name':'alfresco1', 'network':'Clt1-CW-priv1', 'status':'ACTIVE'},
 	             {'name':'mysql1', 'network':'Clt1-TS-priv2', 'status':'ACTIVE'}];
-	
+	$scope.vms=$http.get('vms.json').success(function(data,status,headers,config) {
+		$scope.vms=data;
+	});
 	$scope.step=1;
 	$scope.phase="Ready to start";
 });
--- web/Test1/test/unit/controllersSpec.js
+++ web/Test1/test/unit/controllersSpec.js
@@ -1,27 +1,42 @@
 'use strict';
 describe('Test of controllers - ', function() {
 	describe('VmListCtrl', function() {
-		var scope = {};
-
+		var scope = {},http,httpBackend,controller;
 		beforeEach(function() {
 			module('hybridDemoApp');
-			inject(function($controller) {
+			inject(function($controller,$httpBackend,$http) {
+				
+				httpBackend = $httpBackend;
+				controller=$controller;
+				http=$http;
+          
+				
 
-				$controller('vmListCtrl', {
-					$scope : scope
-				});
 			});
 		});
 
 		it('should return 3 vms', function() {
+			  httpBackend.when("GET", "vms.json").respond([{},{},{}]);	            
+				
+			   httpBackend.expectGET('vms.json');
+				controller('vmListCtrl', {
+					$scope : scope,
+					$http : http 
+				});
+				httpBackend.flush();
 			expect(scope.vms.length).toBe(3);
 		});
 		
 		it('should return json', function() {
-			expect(scope.vms).toEqual([
-			 	             {'name':'apache1', 'network':'Clt1-CW-priv1', 'status':'ACTIVE'},
-				             {'name':'alfresco1', 'network':'Clt1-CW-priv1', 'status':'ACTIVE'},
-				             {'name':'mysql1', 'network':'Clt1-TS-priv2', 'status':'ACTIVE'}]);
+			  httpBackend.when("GET", "vms.json").respond([{'name':'alpha' }]);	            
+			  httpBackend.expectGET('vms.json');
+				
+			 controller('vmListCtrl', {
+				$scope : scope,
+				$http : http 
+			});
+			 httpBackend.flush();
+			expect(scope.vms).toEqual([{'name':'alpha'}]);
 		});
 			
 	});
