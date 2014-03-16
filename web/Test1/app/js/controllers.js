87dbabd3fa1f5ee3a0ccd6dafa4a93ff6f6a69a6
Ben Keith <baynkiz@gmail.com>
2014-03-16 15:04:23 +0100

    repaired

--- web/Test1/app/js/controllers.js
+++ web/Test1/app/js/controllers.js
@@ -3,11 +3,11 @@
  */
 var hybridDemoApp = angular.module('hybridDemoApp',[]);
 
-hybridDemoApp.controller('vmListCtrl',function ($scope,$http) {
+hybridDemoApp.controller('vmListCtrl',function ($scope) {
 	$scope.vms= [
 	             {'name':'apache1', 'network':'Clt1-CW-priv1', 'status':'ACTIVE'},
 	             {'name':'alfresco1', 'network':'Clt1-CW-priv1', 'status':'ACTIVE'},
-	             {'name':'mysql1', 'network':'Clt1-TS-priv2', 'status':'ACTIVE'}]
+	             {'name':'mysql1', 'network':'Clt1-TS-priv2', 'status':'ACTIVE'}];
 	
 	$scope.step=1;
 	$scope.phase="Ready to start";
