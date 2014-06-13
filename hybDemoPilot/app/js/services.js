3e79b0d7a91664a4248c89e176783055cbc4d394
cedric.vanfrachem <cedric.vanfrachem@thalesgroup.com>
2014-06-13 21:48:38 +0200

    localisation relative

--- hybDemoPilot/app/js/services.js
+++ hybDemoPilot/app/js/services.js
@@ -2,7 +2,7 @@ var demoServices = angular.module('demoServices', ['ngResource']);
 
 demoServices.factory('Vm', ['$resource',
   function($resource){
-    return $resource('http://localhost/rest/vms', {}, {
+    return $resource('/rest/vms', {}, {
       query: {method:'GET', params:{}, isArray:true}
     });
   }]);
